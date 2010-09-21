74edcfc3590a918c7b9962ec637344292cb5656d
argoroots <argo@roots.ee>
2010-09-21 11:29:39 +0000

    Vana (PHP) versiooni kustutamine
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@31 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- www/application/views/info/contact.php
+++
@@ -1,4 +0,0 @@
-<h2><?= $this->lang1->str('contact'); ?></h2>
-<p class="text">
-	<?= safe_mailto('info@arx.ee', 'info@arx.ee') ?>
-</p>
--- www/v7/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if($_GET['lang'] == 'eng') {
-	include('v7/tutvustus_eng.php');
-} else {
-	include('v7/tutvustus_est.php');
-}
-
-?>
