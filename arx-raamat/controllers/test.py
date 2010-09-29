d23c8cb3c03a810dcac6a34ad5225869356e1f82
argoroots <argo@roots.ee>
2010-09-29 19:19:57 +0000

    Ticket #11: Apollo importeri otsingu testkontroller
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@49 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/controllers/test.py
+++ arx-raamat/controllers/test.py
@@ -1,17 +1,23 @@
 from bo import *
 from importers.apollo import *
 
-
-class ApolloTest(webapp.RequestHandler):
+class ApolloSearchTest(webapp.RequestHandler):
     def get(self, url):
         #url = '0817496'
+        result = SearchBook(url)
+        self.response.out.write(result)
+
+
+class ApolloBookTest(webapp.RequestHandler):
+    def get(self, url):
         result = GetBookByID(url)
         self.response.out.write(result)
 
 
 def main():
     Route([
-             ('/test/apollo/(.*)', ApolloTest),
+             ('/test/apollo/search/(.*)', ApolloSearchTest),
+             ('/test/apollo/(.*)', ApolloBookTest),
             ])
 
 
