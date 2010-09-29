563d80f15bcf01c951938a0edf9b90fed32ed341
anroots <Andoroots+signup@gmail.com>
2010-09-29 17:13:35 +0000

    Ticket #11: Apollo importer täiendus: oskab otsida raamatut ID järgi. Alustatud raamatu otsinguga parameetrite järgi (nimi,kirjeldus etc)
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@45 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/bo/apollo_importer.py
+++ arx-raamat/bo/apollo_importer.py
@@ -2,11 +2,12 @@
 # Methods to get book information from apollo.ee
 # Dev/maintainer: Ando Roots 2010
 
-import os
 import re
 from google.appengine.api import urlfetch
 from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
+import urllib
+
 
 # Lookup book data from Apollo.ee DB by book ID
 # Input: Book ID
@@ -15,42 +16,80 @@ def GetBookByID(book_id):
     apollo_url = 'http://apollo.ee/product.php/' + book_id
     soup = BeautifulSoup(urlfetch.fetch(apollo_url).content)
     data_object = soup.find('div', attrs={'class' : 'wrapRaamat'})
+    article_object = soup.find('div', attrs={'class' : 'wrapArtikkel'})
     
     # Authors need special parsing - one book can have multiple authors.
     authors = []
     for n in data_object.findAll('a', attrs={'title': 'Veel sellelt autorilt'}):
-        authors.append(''.join(n.findAll(text=True)))
-
-    # Date published
-    position = str(data_object).find('Ilmumisaasta')+12
-    date_published = str(data_object)[(position+1):(position+5)]
-    
-    # ISBN
-    position = str(data_object).find('ISBN-13')+8
-    isbn = str(data_object)[(position):(position+13)]
+        authors.append(''.join(n.findAll(text=True)))    
     
     # This is the main data dict
     data = {'ID': book_id,
             'title': data_object.h2,
             'description': data_object.p,
             'authors': authors,
-            'date_published': date_published,
-            'isbn': isbn}
+            'date_published': FindByPosition(data_object, 'Ilmumisaasta ', 4),
+            'isbn': FindByPosition(data_object, 'ISBN-13 ',13),
+            'publisher': ReSearch(data_object, r'Kirjastus (.*?)<br />'),
+            'price_client': ''.join(soup.find('span', attrs={'class' : 'tooteSoodusHind'})),
+            'price_normal': ''.join(soup.find('span', attrs={'class' : 'tooteHind'})),
+            'format': ReSearch(data_object, r'Formaat (.*?);'),
+            'number_of_pages': ReSearch(data_object, r'Lehekülgi (.*?);'),
+            'img_url': 'http://apollo.ee' + article_object('img')[0]['src']}
     
-    # Trouble with unicode, UTF-8
+    # Trouble with encoding, convert every member of data dict
     nice_data = {}
     for i in data:
         nice_data[i] = ConvertSoup(StripHTML(data[i]))
         
     return nice_data
 
+
+
+# Search Apollo for a book. search_terms is a dict with form data.
+# search_terms example: search_terms = {'name': 'muinasjutud', 'desc': 'lastele'}
+# --- INCOMPLETE! ----
+def SearchBook(search_terms):
+    search_terms['search'] = 'Otsi'
+    search_terms['department'] = '1'
+    
+    search_terms = urllib.urlencode(search_terms)
+    apollo_url = 'http://apollo.ee/advsearch.php?'+search_terms
+    soup = BeautifulSoup(urlfetch.fetch(apollo_url).content)
+    data_object = soup.find('div', attrs={'class' : 'sisuWrap'})
+    
+    return str(data_object)
+
+
+
+
+# Custom search. Get data that follows search_string in the haystack
+# Example: haystack = 'Aasta 1967 talv' | needle = 'sta ' | length = 4
+# 	...would return 1967
+def FindByPosition(haystack, needle, length):
+	position = str(haystack).find(str(needle))+len(needle)
+	return str(haystack)[(position):(position+length)]
+
+
 # Strip out any HTML tags found in input string
 def StripHTML(data):
     data = str(data)
     p = re.compile(r'<.*?>')
     return p.sub('', data)
-    
+
+
+# Regex search
+def ReSearch(haystack, needle):
+	p = re.compile(needle).search(str(haystack))
+	if p:
+		return p.group(1)
+	else:
+		return None
+
+
 # Converts non-English symbols for output
 def ConvertSoup(input):
     result = BeautifulStoneSoup(''.join(input), convertEntities=BeautifulStoneSoup.ALL_ENTITIES)
     return result
+
+# -- End of file -- #
