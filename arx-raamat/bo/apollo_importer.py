e6c480a1eebd309bceb0f1148a1a8be156f60179
anroots <Andoroots+signup@gmail.com>
2010-09-29 14:41:41 +0000

    Ticket #11: Apollo importer täiendus, raamatu info ID järgi, ühilduv GAE-ga
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@44 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/bo/apollo_importer.py
+++ arx-raamat/bo/apollo_importer.py
@@ -1,48 +1,56 @@
+# -*- coding: utf-8 -*-
+# Methods to get book information from apollo.ee
+# Dev/maintainer: Ando Roots 2010
+
 import os
 import re
-import urllib
-import urllib2
+from google.appengine.api import urlfetch
 from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulStoneSoup
 
 # Lookup book data from Apollo.ee DB by book ID
 # Input: Book ID
 # Output: Dict with Apollo's info
 def GetBookByID(book_id):
     apollo_url = 'http://apollo.ee/product.php/' + book_id
-    soup = BeautifulSoup(GetSource(apollo_url))
+    soup = BeautifulSoup(urlfetch.fetch(apollo_url).content)
     data_object = soup.find('div', attrs={'class' : 'wrapRaamat'})
     
-    # Start searching relevant info from data_object and prepare it for returning in dict 'data'
-
     # Authors need special parsing - one book can have multiple authors.
     authors = []
     for n in data_object.findAll('a', attrs={'title': 'Veel sellelt autorilt'}):
         authors.append(''.join(n.findAll(text=True)))
 
     # Date published
-    date_position = str(data_object).find('Ilmumisaasta')+12
-    date_published = str(data_object)[(date_position+1):(date_position+5)]
+    position = str(data_object).find('Ilmumisaasta')+12
+    date_published = str(data_object)[(position+1):(position+5)]
+    
+    # ISBN
+    position = str(data_object).find('ISBN-13')+8
+    isbn = str(data_object)[(position):(position+13)]
     
     # This is the main data dict
     data = {'ID': book_id,
-            'title': ''.join(data_object.h2),
-            'description': ''.join(StripHTML(data_object.p)),
+            'title': data_object.h2,
+            'description': data_object.p,
             'authors': authors,
-            'date_published': date_published}
-    return data
-
-# Return the HTML of input URL
-def GetSource(target_url):
-    headers = { 'User-Agent' : 'Mozilla/5 (Solaris 10) Gecko' }
-    values = {} # Not needed
-    data = urllib.urlencode(values) # Not needed
-    request = urllib2.Request(target_url, data, headers)
-    response = urllib2.urlopen(request)
-    source = response.read()
-    return source
+            'date_published': date_published,
+            'isbn': isbn}
+    
+    # Trouble with unicode, UTF-8
+    nice_data = {}
+    for i in data:
+        nice_data[i] = ConvertSoup(StripHTML(data[i]))
+        
+    return nice_data
 
 # Strip out any HTML tags found in input string
 def StripHTML(data):
     data = str(data)
     p = re.compile(r'<.*?>')
     return p.sub('', data)
+    
+# Converts non-English symbols for output
+def ConvertSoup(input):
+    result = BeautifulStoneSoup(''.join(input), convertEntities=BeautifulStoneSoup.ALL_ENTITIES)
+    return result
