cf47d3d74033860f8f1f4ea21bd43155148b22eb
argoroots <argo@roots.ee>
2010-09-30 12:12:44 +0000

    Ticket #11: Apollo importeri dict väljade ümbernimetamine
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@52 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/importers/apollo.py
+++ arx-raamat/importers/apollo.py
@@ -16,38 +16,39 @@ def GetBookByID(book_id):
     soup = BeautifulSoup(urlfetch.fetch(apollo_url).content)
     data_object = soup.find('div', attrs={'class' : 'wrapRaamat'})
     article_object = soup.find('div', attrs={'class' : 'wrapArtikkel'})
-    
+
     # If there are any editors, put them in a list. If not, leave it be.
     editors = ReSearch(data_object, r'Toimetanud (.*?)<br />')
     if editors:
     	editors = editors.split(',')
-    
+
     # This is the main data dict
-    data = {'id': book_id,
-            'title': data_object.h2,
-            'description': data_object.p,
-            'authors': ParseAuthors(data_object),
-            'date_published': FindByPosition(data_object, 'Ilmumisaasta ', 4),
-            'isbn-10': FindByPosition(data_object, 'ISBN-10 ',10),
-            'isbn-13': FindByPosition(data_object, 'ISBN-13 ',13),
-            'publisher': ReSearch(data_object, r'Kirjastus (.*?)<br />'),
-            'price_client': soup.find('span', attrs={'class' : 'tooteSoodusHind'}),
-            'price_normal': soup.find('span', attrs={'class' : 'tooteHind'}),
-            'series': soup.find('a', attrs={'title' : 'Veel samast sarjast'}),
-            'subtitle': soup.find('h3', attrs={'style' : 'font-weight:normal;'}),
-            'format': ReSearch(data_object, r'Formaat (.*?)(;|<br />)'),
-            'translator': ReSearch(data_object, r'Tõlkinud (.*?)<br />'),
-            'dimensions': ReSearch(data_object, r'Mõõtmed (.*?)<br />'),
-            'editors': editors,
-            'illustrator': ReSearch(data_object, r'Illustreerinud (.*?)<br />'),
-            'number_of_pages': ReSearch(data_object, r'Lehekülgi (.*?);'),
-            'img_url': 'http://apollo.ee' + article_object('img')[0]['src']}
-    
+    data = {
+        'id': book_id,
+        'isbn10': FindByPosition(data_object, 'ISBN-10 ',10),
+        'isbn13': FindByPosition(data_object, 'ISBN-13 ',13),
+        'title': data_object.h2,
+        'subtitle': soup.find('h3', attrs={'style' : 'font-weight:normal;'}),
+        'series': soup.find('a', attrs={'title' : 'Veel samast sarjast'}),
+        'description': data_object.p,
+        'authors': ParseAuthors(data_object),
+        'publisher': ReSearch(data_object, r'Kirjastus (.*?)<br />'),
+        'published': FindByPosition(data_object, 'Ilmumisaasta ', 4),
+        'translator': ReSearch(data_object, r'Tõlkinud (.*?)<br />'),
+        'editors': editors,
+        'illustrator': ReSearch(data_object, r'Illustreerinud (.*?)<br />'),
+        'price': soup.find('span', attrs={'class' : 'tooteHind'}),
+        'format': ReSearch(data_object, r'Formaat (.*?)(;|<br />)'),
+        'dimensions': ReSearch(data_object, r'Mõõtmed (.*?)<br />'),
+        'pages': ReSearch(data_object, r'Lehekülgi (.*?);'),
+        'image': 'http://apollo.ee' + article_object('img')[0]['src'],
+    }
+
     # Trouble with encoding, convert every member of data dict
     nice_data = {}
     for i in data:
         nice_data[i] = ConvertSoup(StripHTML(data[i]))
-        
+
     return nice_data
 
 
@@ -56,31 +57,33 @@ def GetBookByID(book_id):
 # Input: String search_term
 # Output: a list with nested dict-s, one for each result
 def SearchBook(search_term):
-    
+
     # Post the search and get the HTML of the result page.
     apollo_url = 'http://apollo.ee/search.php?keyword='+search_term
     soup = BeautifulSoup(urlfetch.fetch(apollo_url).content)
     result_block = soup.find('div', attrs={'class' : 'otsingTulemusRaamat'})
-    
+
     # Since the results are poorly structured we have to split it into individual parts.
     data_objects = str(result_block).split('<div class="sisuSplitter">&nbsp;</div>')
     data = []
     for data_object in data_objects:
     	current_soup = BeautifulSoup(data_object)
-    	
+
     	# Primary data gathering
-    	current_info = {'id': ReSearch(current_soup, r'php/(.*?)"'),
-    					'title': current_soup.find('a'),
-    					'description': current_soup.find('p'),
-    					'authors': ParseAuthors(current_soup)}
-    	
+    	current_info = {
+            'id': ReSearch(current_soup, r'php/(.*?)"'),
+            'title': current_soup.find('a'),
+    		'description': current_soup.find('p'),
+    		'authors': ParseAuthors(current_soup),
+        }
+
     	# Get around UTF-8 problems
     	nice_current_info = {}
     	for i in current_info:
     		nice_current_info[i] = ConvertSoup(StripHTML(current_info[i]))
     	data.append(nice_current_info)
     	nice_current_info = None
-    
+
     return str(data)
 
 # ------------------------------ Helper functions --------------------------------- #
