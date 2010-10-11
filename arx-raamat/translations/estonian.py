a7e65be2d1a25f5187abda7fe79f60686e5a8c63
argoroots <argo@roots.ee>
2010-10-11 15:53:48 +0000

    HTML5; Blueprint CSS'ist loobumine;
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@56 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/translations/estonian.py
+++ arx-raamat/translations/estonian.py
@@ -2,10 +2,11 @@
 
 def translation():
     return {
-    	'':					'',
-    	'apollo_failure_msg':       'Hei Admin...\nGAE Cron teavitab sind sellest, et ARXi Apollo importija läks katki.\nKontrolli Apollo HTML syntaxit käsitsi.\n Teate saatis /importers/apollo_cron.py',
-    	'apollo_failure_msg_title':              'ARX Apollo Importer Katki',
+    	'':					    '',
+    	'apollo_failure_msg':   'Hei Admin...\nGAE Cron teavitab sind sellest, et ARXi Apollo importija läks katki.\n Kontrolli Apollo HTML syntaxit käsitsi.\n Teate saatis /importers/apollo_cron.py',
+    	'apollo_failure_msg_title': 'ARX Apollo Importer Katki',
         'catalog':              'Kataloog',
+        'debtors':              'Võlglased',
         'english':              'Inglise',
         'estonian':             'Eesti',
         'groups':               'Grupid',
@@ -16,6 +17,7 @@ def translation():
         'logout':               'Välju',
         'ordering':             'Tellimine',
         'persons':              'Isikud',
+        'persons_page_info':    'Siin lehel saad hallata raamatukoguga seotud isikuid. Isikutele saab laenutada teavikuid ning neid saab kaasata raamatukogu töös. Isikuid saab jagada gruppidesse.',
         'preferences':          'Seaded',
         'save':                 'Salvesta',
         'search':               'Otsi',
