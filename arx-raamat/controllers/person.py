34347c9b8d2af5bd4f31d46f0c6e77ffd60c6f37
argoroots <argo@roots.ee>
2010-11-11 16:33:44 +0000

    Persoonide vaade
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@75 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/controllers/person.py
+++ arx-raamat/controllers/person.py
@@ -1,14 +1,91 @@
 from bo import *
+from database import *
 
 
-class Frontpage(webapp.RequestHandler):
+class ShowStaff(webapp.RequestHandler):
     def get(self):
-        View(self, '', 'person.html')
+
+        groups = db.Query(Group)
+        groups.filter('library', Person().current.library)
+        groups.order('name')
+        groups.fetch(100)
+
+        persons = db.Query(Person)
+        persons.filter('library', Person().current.library)
+        persons.filter('status', 'normal')
+        persons.filter('type IN', ['staff', 'admin'])
+        persons.order('forename')
+        persons.order('surname')
+        persons.fetch(1000)
+
+        View(self, '', 'person.html', {
+            'groups': groups,
+            'persons': persons,
+            'selected': 'staff',
+        })
+
+
+class ShowDebtors(webapp.RequestHandler):
+    def get(self):
+        pass
+
+
+class ShowGroup(webapp.RequestHandler):
+    def get(self, url):
+
+        url = url.strip('/')
+
+        groups = db.Query(Group)
+        groups.filter('library', Person().current.library)
+        groups.order('name')
+        groups.fetch(100)
+
+        persons = db.Query(Person)
+        persons.filter('library', Person().current.library)
+        persons.filter('status', 'normal')
+        persons.filter('group', Group().get_by_id(int(url[1])))
+        persons.order('forename')
+        persons.order('surname')
+        persons.fetch(1000)
+
+        View(self, '', 'person.html', {
+            'groups': groups,
+            'persons': persons,
+            'selected': url,
+        })
+
+
+class ShowPerson(webapp.RequestHandler):
+    def get(self, url):
+
+        url = url.strip('/')
+
+        groups = db.Query(Group)
+        groups.filter('library', Person().current.library)
+        groups.order('name')
+        groups.fetch(100)
+
+        persons = db.Query(Person)
+        persons.filter('library', Person().current.library)
+        persons.filter('status', 'normal')
+        #persons.filter('group', Group().get_by_id(int(url[1])))
+        persons.order('forename')
+        persons.order('surname')
+        persons.fetch(1000)
+
+        View(self, '', 'person.html', {
+            'groups': groups,
+            'persons': persons,
+            'selected': url,
+        })
 
 
 def main():
     Route([
-             ('/person', Frontpage),
+             ('/person/staff', ShowStaff),
+             ('/person/debtors', ShowDebtors),
+             (r'/person/group(.*)', ShowGroup),
+             (r'/person(.*)', ShowPerson),
             ])
 
 
