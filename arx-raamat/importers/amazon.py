0a4141030ecbda07c7bfa463d1e8ab43adb7129e
anroots <Andoroots+signup@gmail.com>
2010-11-13 12:56:19 +0000

    Amazon importeri II versioon. Mõlemad funktsioonid - SearchBook() ja GetBook töötavad rahuldavalt, kuid vajavad testimist. Otsides tagastatakse vähe infot, GetBook korral rohkem, koos pildiga.
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@78 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/importers/amazon.py
+++ arx-raamat/importers/amazon.py
@@ -17,23 +17,24 @@ search_book_details = {} # Holds the details of a single search result
 # Returns a list containing dicts for each result
 def SearchBook(search_term):
     amazon = Amazon(ACCESS_KEY_ID, SECRET_ACCESS_KEY, ASSOCIATE_TAG)
-    xml = amazon.ItemSearch(SearchIndex = 'Books', ResponseGroup = 'Medium', Keywords = search_term)
+    xml = amazon.ItemSearch(SearchIndex = 'Books', ResponseGroup = 'Small', Keywords = search_term)
     #f = open('../importers/SearchBook.xml')
     #xml = f.read()
     #f.close
-    
     dom = minidom.parseString(xml)
 
     final_data = []
     for search_book_item in dom.getElementsByTagName('Item'):
-    	
+        
         global search_book_item
         global search_book_details
         
         addSearchDetails('Title')
         addSearchDetails('Author', True)
-        addSearchDetails('ISBN')
+        #addSearchDetails('ISBN') ISBN-i ei ole ResponseGroup=Small
         addSearchDetails('ASIN')
+        addSearchDetails('DetailPageURL')
+        addSearchDetails('Manufacturer')
         
         
         final_data.append(search_book_details)
@@ -46,12 +47,12 @@ def SearchBook(search_term):
 # Search by ASIN
 def GetBook(ASIN):
     amazon = Amazon(ACCESS_KEY_ID, SECRET_ACCESS_KEY, ASSOCIATE_TAG)
-    xml = amazon.ItemLookup(ItemId=ASIN, IdType='ASIN', ResponseGroup='ItemAttributes')
+    xml = amazon.ItemLookup(ItemId=ASIN, IdType='ASIN', ResponseGroup='Large')
     dom = minidom.parseString(xml)
-    
+	
     # The for loop is potentially unnecessary, but I haven't found a way to remove it.
-    for get_book_item in dom.getElementsByTagName('Item'):
-        global get_book_item
+    get_book_item = dom.getElementsByTagName('Item')[0]
+    global get_book_item
     
     # What data to return? The argument indicates an XML tag name
     # If the tag is not found, the entry will be ignored
@@ -63,16 +64,31 @@ def GetBook(ASIN):
     addDetails('Manufacturer')
     addDetails('ReleaseDate')
     addDetails('Studio')
-    addDetails('ReadingLevel')
+    addDetails('DetailPageURL')
+    addDetails('Binding')
+    addDetails('Publisher')
+    get_book_details['LargeImage'] = getImage()
     
     return get_book_details
 
 
 # Adds an additional return value to the GetBook query
 def addDetails(tag_name, many = False):
+    global get_book_item
     data = getElement(get_book_item, tag_name, many)
     if data:
         get_book_details[tag_name] = data
+
+
+# Returns the largest available image of the item.
+def getImage():
+	global get_book_item
+	try:
+		large_image = get_book_item.getElementsByTagName('LargeImage')[0].getElementsByTagName('URL')[0].firstChild.data
+		return large_image
+	except IndexError, e:
+		return None
+
 
 # Add an additional return value to the SearchBook query
 def addSearchDetails(tag_name, many = False):
@@ -96,22 +112,10 @@ def getElement(haystack, tag, many = False):
             if (len(data) == 1):
                 return data[0]
             else:
-                return data[0] + ' <font color="red">and </font>' + data[1]
+                return data[0] + ' <font color="red">and </font>' + data[1] # For testing only
         else:
             return element[0].firstChild.data
     except IndexError, e:
         return None
 
-# 
-#print SearchBook('Alyson Noel')
-# 
-# for item in m:
-#     for i in item:
-#         print i + ' => ' + item[i]
-#         print ''
-# print '--------------------------------'
-#GetBook('0439358078')
-# for item in m:
-#     for i in item:
-#         print i + ' => ' + item[i]
-#         print ''
+# End of file amazon.py #
