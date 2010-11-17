3d4aaca75631b79288a029dbc1247631ac2efd02
argoroots <argo@roots.ee>
2010-11-17 15:07:02 +0000

    Inglise ja venekeelne tõlge; Google Translate tõlkemoodul
    git-svn-id: http://argo.unfuddle.com/svn/argo_arx/trunk@81 25de2f3d-7254-4139-90b3-6d3fdcc88159

--- arx-raamat/controllers/admin.py
+++ arx-raamat/controllers/admin.py
@@ -1,5 +1,8 @@
+import urllib
 import hashlib
 from time import time
+from google.appengine.api import urlfetch
+from django.utils import simplejson
 
 from bo import *
 from database import *
@@ -52,11 +55,44 @@ class SendInvites(webapp.RequestHandler):
         self.redirect('/admin')
 
 
+class TranslateAll(webapp.RequestHandler):
+    def get(self, from_lang, to_lang):
+
+        languagecodes = {
+            'et': 'estonian',
+            'en': 'english',
+            'ru': 'russian',
+        }
+        api_key = 'AIzaSyDeBz3bFbw2mdOkZwi3cRpjyWqeBjOX4IY'
+        translations = {}
+
+        if from_lang in languagecodes:
+            l = __import__('translations.' + languagecodes[from_lang], globals(), locals(), ['translation'], -1)
+
+            for key, value in l.translation().iteritems():
+                url = 'https://www.googleapis.com/language/translate/v2?format=html&key=' + api_key + '&q=' + urllib.quote(value) + '&source=' + from_lang + '&target=' + to_lang
+                json = simplejson.loads(urlfetch.fetch(url, deadline=10).content)
+                if 'data' in json:
+                    translations[key] = json['data']['translations'][0]['translatedText']
+                else:
+                    translations[key] = 'x'
+
+            self.response.out.write('# -*- coding: utf-8 -*-\n\n')
+            self.response.out.write('def translation():\n')
+            self.response.out.write('   return {\n')
+            for key, value in translations.iteritems():
+                self.response.out.write('        \'' + key + '\': \'' + value + '\',\n')
+            self.response.out.write('}')
+        else:
+            self.response.out.write('ERROR: No language "' + from_lang + '"')
+
+
 def main():
     Route([
              ('/admin', ShowAdmin),
              ('/admin/library', AddLibrary),
              ('/admin/invite', SendInvites),
+             ('/admin/translate/(.*)/(.*)', TranslateAll),
             ])
 
 
